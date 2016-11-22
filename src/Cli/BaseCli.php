d07d68521b66afc0986d7a2c192be8b551bfc505
dongwenqiang <dongwenqiang@elex-tech.com>
2016-11-22 13:59:36 +0800

    client

---
+++ src/Cli/BaseCli.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dong
+ * Date: 16/11/22
+ * Time: 下午1:54
+ */
+
+namespace Serverx\Cli;
+
+
+class BaseCli
+{
+
+}
---
+++ src/Cli/TCP.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dong
+ * Date: 16/11/22
+ * Time: 下午1:58
+ */
+
+namespace Serverx\Cli;
+
+
+class TCP extends BaseCli
+{
+
+    
+}
